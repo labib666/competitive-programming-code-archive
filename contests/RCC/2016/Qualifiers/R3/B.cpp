8370fd7b5783f83c0ade4d7f12c6a6fb326f64cb
Labib666 <ewmahadicd@gmail.com>
2016-06-05 23:30:43 +0600

    Russian Code Cup codes

---
+++ contests/RCC/2016/Qualifiers/R3/B.cpp
@@ -0,0 +1,144 @@
+// B/***********Template Starts Here***********/
+//#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <functional>
+#include <string>
+#include <iostream>
+#include <cctype>
+#include <set>
+#include <climits>
+#include <iomanip>
+#include <cassert>
+#include <sstream>
+//#include <unordered_map>
+
+#define pb push_back
+#define nl puts ("")
+#define sp printf ( " " )
+#define phl printf ( "hello\n" )
+#define ff first
+#define ss second
+#define POPCOUNT __builtin_popcountll
+#define RIGHTMOST __builtin_ctzll
+#define LEFTMOST(x) (63-__builtin_clzll((x)))
+#define MP make_pair
+#define FOR(i,x,y) for(int i = (x) ; i <= (y) ; ++i)
+#define ROF(i,x,y) for(int i = (y) ; i >= (x) ; --i)
+#define CLR(x,y) memset(x,y,sizeof(x))
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define NUMDIGIT(x,y) (((int)(log10((x))/log10((y))))+1)
+#define SQ(x) ((x)*(x))
+#define ABS(x) ((x)<0?-(x):(x))
+#define FABS(x) ((x)+eps<0?-(x):(x))
+#define ALL(x) (x).begin(),(x).end()
+#define LCM(x,y) (((x)/gcd((x),(y)))*(y))
+#define SZ(x) ((int)(x).size())
+
+using namespace std;
+
+#define LL long long
+typedef long long vlong;
+typedef unsigned long long uvlong;
+typedef pair < int, int > pii;
+typedef pair < vlong, vlong > pll;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+
+const vlong inf = 2147383647;
+const vlong mod = 1000000007;
+const double pi = 2 * acos ( 0.0 );
+const double eps = 1e-9;
+
+#ifdef forthright48
+     #include <ctime>
+     clock_t tStart = clock();
+     #define debug(args...) {dbg,args; cerr<<endl;}
+     #define timeStamp printf("Execution Time: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+#else
+    #define debug(args...)  // Just strip off all debug tokens
+    #define timeStamp
+#endif
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+//int knightDir[8][2] = { {-2,1},{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,-1} };
+//int dir4[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+
+
+inline vlong gcd ( vlong a, vlong b ) {
+    a = ABS ( a ); b = ABS ( b );
+    while ( b ) { a = a % b; swap ( a, b ); } return a;
+}
+
+vlong ext_gcd ( vlong A, vlong B, vlong *X, vlong *Y ){
+    vlong x, y, u, v, m, n, a, b, q, r;
+    x = 0; y = 1;
+    u = 1; v = 0;
+    for (a = A, b = B; 0 != a; b = a, a = r, x = u, y = v, u = m, v = n) {
+        q = b / a;
+        r = b % a;
+        m = x - (u * q);
+        n = y - (v * q);
+    }
+    *X = x; *Y = y;
+    return b;
+}
+
+inline vlong modInv ( vlong a, vlong m ) {
+    vlong x, y;
+    ext_gcd( a, m, &x, &y );
+    if ( x < 0 ) x += m; //modInv is never negative
+    return x;
+}
+
+inline vlong power ( vlong a, vlong p ) {
+    vlong res = 1, x = a;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x );
+        x = ( x * x ); p >>= 1;
+    }
+    return res;
+}
+
+inline vlong bigmod ( vlong a, vlong p, vlong m ) {
+    vlong res = 1 % m, x = a % m;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x ) % m;
+        x = ( x * x ) % m; p >>= 1;
+    }
+    return res;
+}
+
+/***********Template Ends Here***********/
+vlong t, tc = 0;vlong n;char s1[100004], s2[100004];
+vlong dp[100005][4];void convert (vlong typ, vlong &b1, vlong &b2) {	if (typ == 0) return;	if (typ == 1) {		b1 ^= 1;		return;	}	if (typ == 2) {		b2 ^= 1;		return;	}	if (typ == 3) {		b1 ^= 1;		b2 ^= 1;		return;	} return;}vlong solve(vlong pos, vlong last) {	vlong b1 = s1[pos]-'0';	vlong b2 = s2[pos]-'0';	convert(last,b1,b2);	if (pos == n-1) {		return (b1 == b2) ? 0 : inf;	}	if (dp[pos][last] != -1) return dp[pos][last];	dp[pos][last] = inf;	if (b1 == b2) {		dp[pos][last] = MIN(dp[pos][last], solve(pos+1,0));		dp[pos][last] = MIN(dp[pos][last], solve(pos+1,3)+2);	}	else {		dp[pos][last] = MIN(dp[pos][last], solve(pos+1,1)+1);		dp[pos][last] = MIN(dp[pos][last], solve(pos+1,2)+1);	}	return dp[pos][last];}
+
+int main () {
+    #ifdef forthright48
+    freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+
+	scanf("%lld", &t);	while (tc<t) {		tc++;		scanf("%lld", &n);		scanf("%s", s1);		scanf("%s", s2);		for (vlong i=0; i<=n; i++) {			dp[i][0] = dp[i][1] = dp[i][2] = dp[i][3] = -1;		}		vlong ans = solve(0,0);		if (ans >= inf) {			printf("-1\n");		}		else {			printf("%lld\n", ans);		}	}
+
+
+    return 0;
+}
---
+++ contests/RCC/2016/Qualifiers/R3/C.cpp
@@ -0,0 +1,138 @@
+// C/***********Template Starts Here***********/
+//#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <deque>
+#include <functional>
+#include <string>
+#include <iostream>
+#include <cctype>
+#include <set>
+#include <climits>
+#include <iomanip>
+#include <cassert>
+#include <sstream>
+//#include <unordered_map>
+
+#define pb push_back
+#define nl puts ("")
+#define sp printf ( " " )
+#define phl printf ( "hello\n" )
+#define ff first
+#define ss second
+#define POPCOUNT __builtin_popcountll
+#define RIGHTMOST __builtin_ctzll
+#define LEFTMOST(x) (63-__builtin_clzll((x)))
+#define MP make_pair
+#define FOR(i,x,y) for(int i = (x) ; i <= (y) ; ++i)
+#define ROF(i,x,y) for(int i = (y) ; i >= (x) ; --i)
+#define CLR(x,y) memset(x,y,sizeof(x))
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define NUMDIGIT(x,y) (((int)(log10((x))/log10((y))))+1)
+#define SQ(x) ((x)*(x))
+#define ABS(x) ((x)<0?-(x):(x))
+#define FABS(x) ((x)+eps<0?-(x):(x))
+#define ALL(x) (x).begin(),(x).end()
+#define LCM(x,y) (((x)/gcd((x),(y)))*(y))
+#define SZ(x) ((int)(x).size())
+
+using namespace std;
+
+#define LL long long
+typedef long long vlong;
+typedef unsigned long long uvlong;
+typedef pair < int, int > pii;
+typedef pair < vlong, vlong > pll;
+typedef vector<pii> vii;
+typedef vector<int> vi;
+
+const vlong inf = 2147383647;
+const vlong mod = 1000000007;
+const double pi = 2 * acos ( 0.0 );
+const double eps = 1e-9;
+
+#ifdef forthright48
+     #include <ctime>
+     clock_t tStart = clock();
+     #define debug(args...) {dbg,args; cerr<<endl;}
+     #define timeStamp printf("Execution Time: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+#else
+    #define debug(args...)  // Just strip off all debug tokens
+    #define timeStamp
+#endif
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+//int knightDir[8][2] = { {-2,1},{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,-1} };
+//int dir4[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+
+
+inline vlong gcd ( vlong a, vlong b ) {
+    a = ABS ( a ); b = ABS ( b );
+    while ( b ) { a = a % b; swap ( a, b ); } return a;
+}
+
+vlong ext_gcd ( vlong A, vlong B, vlong *X, vlong *Y ){
+    vlong x, y, u, v, m, n, a, b, q, r;
+    x = 0; y = 1;
+    u = 1; v = 0;
+    for (a = A, b = B; 0 != a; b = a, a = r, x = u, y = v, u = m, v = n) {
+        q = b / a;
+        r = b % a;
+        m = x - (u * q);
+        n = y - (v * q);
+    }
+    *X = x; *Y = y;
+    return b;
+}
+
+inline vlong modInv ( vlong a, vlong m ) {
+    vlong x, y;
+    ext_gcd( a, m, &x, &y );
+    if ( x < 0 ) x += m; //modInv is never negative
+    return x;
+}
+
+inline vlong power ( vlong a, vlong p ) {
+    vlong res = 1, x = a;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x );
+        x = ( x * x ); p >>= 1;
+    }
+    return res;
+}
+
+inline vlong bigmod ( vlong a, vlong p, vlong m ) {
+    vlong res = 1 % m, x = a % m;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x ) % m;
+        x = ( x * x ) % m; p >>= 1;
+    }
+    return res;
+}
+
+/***********Template Ends Here***********/
+
+#define root 0#define N 10100#define LN 17vector <int> adj[N], costs[N], indexx[N];int baseArray[N], ptr;int chainNo, chainInd[N], chainHead[N], posInBase[N];int depth[N], pa[LN][N], otherEnd[N], subsize[N];int st[N*6], qt[N*6];/* * make_tree: * Used to construct the segment tree. It uses the baseArray for construction */void make_tree(int cur, int s, int e) {	if(s == e-1) {		st[cur] = baseArray[s];		return;	}	int c1 = (cur<<1), c2 = c1 | 1, m = (s+e)>>1;	make_tree(c1, s, m);	make_tree(c2, m, e);	st[cur] =  st[c1] + st[c2];}/* * update_tree: * Point update. Update a single element of the segment tree. */void update_tree(int cur, int s, int e, int x, int val) {	if(s > x || e <= x) return;	if(s == x && s == e-1) {		st[cur] = val;		return;	}	int c1 = (cur<<1), c2 = c1 | 1, m = (s+e)>>1;	update_tree(c1, s, m, x, val);	update_tree(c2, m, e, x, val);	st[cur] = st[c1] + st[c2];}/* * query_tree: * Given S and E, it will return the maximum value in the range [S,E) */void query_tree(int cur, int s, int e, int S, int E) {	if(s >= E || e <= S) {		qt[cur] = 0;		return;	}	if(s >= S && e <= E) {		qt[cur] = st[cur];		return;	}	int c1 = (cur<<1), c2 = c1 | 1, m = (s+e)>>1;	query_tree(c1, s, m, S, E);	query_tree(c2, m, e, S, E);	qt[cur] = qt[c1] + qt[c2];}/* * query_up: * It takes two nodes u and v, condition is that v is an ancestor of u * We query the chain in which u is present till chain head, then move to next chain up * We do that way till u and v are in the same chain, we query for that part of chain and break */int query_up(int u, int v) {	if(u == v) return 0; // Trivial	int uchain, vchain = chainInd[v], ans = 0;	// uchain and vchain are chain numbers of u and v	while(1) {		uchain = chainInd[u];		if(uchain == vchain) {			// Both u and v are in the same chain, so we need to query from u to v, update answer and break.			// We break because we came from u up till v, we are done			if(u==v) break;			query_tree(1, 0, ptr, posInBase[v]+1, posInBase[u]+1);			// Above is call to segment tree query function			ans += qt[1]; // Update answer			break;		}		query_tree(1, 0, ptr, posInBase[chainHead[uchain]], posInBase[u]+1);		// Above is call to segment tree query function. We do from chainHead of u till u. That is the whole chain from		// start till head. We then update the answer		ans += qt[1];		u = chainHead[uchain]; // move u to u's chainHead		u = pa[0][u]; //Then move to its parent, that means we changed chains	}	return ans;}/* * LCA: * Takes two nodes u, v and returns Lowest Common Ancestor of u, v */int LCA(int u, int v) {	if(depth[u] < depth[v]) swap(u,v);	int diff = depth[u] - depth[v];	for(int i=0; i<LN; i++) if( (diff>>i)&1 ) u = pa[i][u];	if(u == v) return u;	for(int i=LN-1; i>=0; i--) if(pa[i][u] != pa[i][v]) {		u = pa[i][u];		v = pa[i][v];	}	return pa[0][u];}void query(int u, int v) {	/*	 * We have a query from u to v, we break it into two queries, u to LCA(u,v) and LCA(u,v) to v	 */	int lca = LCA(u, v);	int ans = query_up(u, lca); // One part of path	int temp = query_up(v, lca); // another part of path	ans += temp; // take the maximum of both paths	printf("%d\n", ans);}/* * change: * We just need to find its position in segment tree and update it */void change(int u, int val) {	//int u = otherEnd[i];	//debug(u, posInBase[u], val);	update_tree(1, 0, ptr, posInBase[u], val);}/* * Actual HL-Decomposition part * Initially all entries of chainHead[] are set to -1. * So when ever a new chain is started, chain head is correctly assigned. * As we add a new node to chain, we will note its position in the baseArray. * In the first for loop we find the child node which has maximum sub-tree size. * The following if condition is failed for leaf nodes. * When the if condition passes, we expand the chain to special child. * In the second for loop we recursively call the function on all normal nodes. * chainNo++ ensures that we are creating a new chain for each normal child. */void HLD(int curNode, int cost, int prev) {	if(chainHead[chainNo] == -1) {		chainHead[chainNo] = curNode; // Assign chain head	}	//debug(curNode, cost, prev, chainNo);	chainInd[curNode] = chainNo;	posInBase[curNode] = ptr; // Position of this node in baseArray which we will use in Segtree	baseArray[ptr++] = cost;	int sc = -1, ncost;	// Loop to find special child	for(int i=0; i<adj[curNode].size(); i++) {		if(adj[curNode][i] != prev) {			if(sc == -1 || subsize[sc] < subsize[adj[curNode][i]]) {				sc = adj[curNode][i];				ncost = costs[curNode][i];			}		}	}	if(sc != -1) {		// Expand the chain		HLD(sc, ncost, curNode);	}	for(int i=0; i<adj[curNode].size(); i++) if(adj[curNode][i] != prev) {		if(sc != adj[curNode][i]) {			// New chains at each normal node			chainNo++;			HLD(adj[curNode][i], costs[curNode][i], curNode);		}	}}/* * dfs used to set parent of a node, depth of a node, subtree size of a node */void dfs(int cur, int prev, int _depth=0) {	//debug(cur, prev, _depth);	pa[0][cur] = prev;	depth[cur] = _depth;	subsize[cur] = 1;	for(int i=0; i<adj[cur].size(); i++) {		//debug("edge", cur, adj[cur][i]);		if(adj[cur][i] != prev) {			otherEnd[indexx[cur][i]] = adj[cur][i];			dfs(adj[cur][i], cur, _depth+1);			subsize[cur] += subsize[adj[cur][i]];		}	}}int main() {	#ifdef forthright48
+    freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    //ios_base::sync_with_stdio(0);
+    //cin.tie(0);
+
+	int t = 1;	//scanf("%d ", &t);	while(t--) {		ptr = 0;		int n;		scanf("%d", &n);		for(int i=0; i<n; i++) {			adj[i].clear();			costs[i].clear();			indexx[i].clear();			chainHead[i] = -1;			for(int j=0; j<LN; j++) pa[j][i] = -1;		}		for(int i=2; i<=n; i++) {			int u, v, c;			scanf("%d", &u);			v = i; c = 1;			u--; v--;			adj[u].push_back(v);			costs[u].push_back(c);			indexx[u].push_back(i-1);			adj[v].push_back(u);			costs[v].push_back(c);			indexx[v].push_back(i-1);		}		chainNo = 0;		dfs(root, -1); // We set up subsize, depth and parent for each node		HLD(root, -1, -1); // We decomposed the tree and created baseArray		make_tree(1, 0, ptr); // We use baseArray and construct the needed segment tree		// Below Dynamic programming code is for LCA.		for(int i=1; i<LN; i++){			for(int j=0; j<n; j++){				if(pa[i-1][j] != -1){					pa[i][j] = pa[i-1][pa[i-1][j]];				}			}		}		int q;		scanf("%d", &q);		while(q--) {			int qtyp;			scanf("%d", &qtyp);			int a, b;			scanf("%d", &a);			//debug(qtyp, a);			if(qtyp == 1) {				scanf("%d", &b);				query(a-1, b-1);			}			else {				change(a-1, 0);			}		}	}}
+
